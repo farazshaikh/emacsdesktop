63b19ac1bda28ee17829496314d9cbd1d2a24808
faraz <faraz.shaikh@gmail.com>
2020-10-11 21:36:34 -0700

    wallpapers

--- emacs/installexwm.sh
+++ emacs/installexwm.sh
@@ -112,7 +112,7 @@ linkupFiles() {
     linkup ${ts} ~/.xinitrc `pwd`/emacs/.xinitrc
     linkup ${ts} ~/.Xresources `pwd`/emacs/.Xresources
     linkup ${ts} ~/ediff.sh `pwd`/emacs/ediff.sh
-    linkup ${ts} ~/wallpaper.jpg `pwd`/emacs/wallpaper.jpg
+    linkup ${ts} ~/wallpaper.jpg `pwd`/wallpapers/wallpaper.jpg
 
     linkup ${ts} /etc/X11/Xsession.d/10-retina-display `pwd`/emacs/etc_X11_Xsession.d_10-retina-display +
     linkup ${ts} /etc/X11/Xresources/retina-display `pwd`/emacs/etc_X11_Xresources_retina-display +
