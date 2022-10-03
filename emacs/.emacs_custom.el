4372de1b722b675786b391ae51c14fa75542a9f5
faraz@ <faraz@faraz>
2022-10-02 18:42:42 -0700

    format buffer & remove custom font settings

--- emacs/.emacs_custom.el
+++ emacs/.emacs_custom.el
@@ -17,13 +17,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Source Code Pro"))))
- '(ivy-current-match ((t (:background "#2d2e2e" :box (:line-width 2 :color "grey75" :style released-button)))))
  '(ivy-posframe-border ((t (:inherit internal-border :background "white" :foreground "white"))))
- '(lsp-ui-doc-background ((t (:background nil))))
- '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
- '(markdown-code-face ((t (:inherit nil))))
- '(spaceline-highlight-face ((t (:foreground "black")))))
+ '(ivy-current-match ((t (:background "#2d2e2e" :box (:line-width 2 :color "grey75" :style released-button)))))
+ '(default ((t (:family "Source Code Pro"))))
+)
 
 (message "es/customizations-font-applied")
 (provide 'emacs_custom)
