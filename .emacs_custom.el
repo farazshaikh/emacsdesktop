19fcb8278af00c800c672014321838cca1219663
faraz <faraz.shaikh@gmail.com>
2024-01-11 22:44:18 -0800

    use whitespace package for fill column indicator

---
+++ .emacs_custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((flycheck-disabled-checkers . emacs-lisp-checkdoc)))
+ '(package-selected-packages
+   '(company-solidity flymake-ktlint flymake-solidity solidity-flycheck solidity-mode flexoki-themes grey-paper-theme paper-theme doom-themes clipetty treemacs company-prescient corfu-prescient keycast lsp-ltex flycheck-grammarly lsp-grammarly grammarly pdf-tools latex-preview-pane lsp-latex toc-org org-bullets org-superstar toml-mode company-ansible ansible yaml-mode py-autopep8 go-autocomplete lsp-haskell haskell-mode cargo flycheck-rust rust-mode clang-format+ clang-format company-c-headers kind-icon corfu-terminal corfu company-posframe company-box company-quickhelp company orderless lsp-ivy lsp-ui lsp-mode popup yasnippet-snippets counsel-projectile direnv magit git-timemachine git-gutter flycheck flyspell-correct-ivy persistent-scratch ssh-agency rg marginalia ivy-hydra counsel flx windower dedicated doom-modeline all-the-icons-dired unicode-fonts modus-themes vterm adoc-mode default-text-scale writeroom-mode vertico-posframe use-package-hydra quelpa-use-package diminish consult))
+ '(safe-local-variable-values '((flycheck-disabled-checkers . emacs-lisp-checkdoc)))
+ '(warning-suppress-log-types
+   '((lsp-mode)))
+ '(warning-suppress-types
+   '((lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-code-face ((t (:inherit default)))))
